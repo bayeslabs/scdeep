656a585439aaeb0aa4c215521997c407e4486b27
unknown <sarawgi.nikita@gmail.com>
2020-07-09 00:05:37 +0530

    packaging

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+VERSION = "0.0.1"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="deepscan",
+    version=VERSION,
+    author="BayesLabs",
+    author_email="contact@bayeslabs.co",
+    description="single-cell analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bayeslabs/Deepscan",
+    packages=setuptools.find_packages(),
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
